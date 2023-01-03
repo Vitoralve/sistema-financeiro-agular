351b46d93d8f4be25e25754d8eb57480aaa0fc6c
Vitor Alves <vitoralve81@gmail.com]>
2023-01-03 19:17:46 -0300

    configurando pastas inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      PanelComponent
+   ],
   imports: [
     BrowserModule
   ],
